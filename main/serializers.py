985fd923425b146c89fd57cf00b5b21b24feac48
Radia Zaman <radiazaman798@gmail.com>
2026-03-01 17:36:54 +0600

    Serializer Fix

--- main/serializers.py
+++ main/serializers.py
@@ -12,16 +12,25 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = '__all__'
+        fields = fields = ['id', 'user', 'text', 'created_at']
 
 class PostSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-    likes_count = serializers.SerializerMethodField()
+    user = serializers.StringRelatedField(read_only=True)
     comments = CommentSerializer(many=True, read_only=True)
+    likes_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = [
+            'id',
+            'user',
+            'text',
+            'image',
+            'youtube_url',
+            'created_at',
+            'likes_count',
+            'comments',
+        ]
 
     def get_likes_count(self, obj):
         return obj.likes.count()
